5245b5c7a71dbd2718ed26a934ea084a6602ab51
Karan Shah <karanshah0206@outlook.com>
2020-07-17 16:00:03 +0530

    Client-side User Login + Register With Basic UI

---
+++ DatingApp-SPA/src/app/nav/nav.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../_services/auth.service';
+
+@Component({
+  selector: 'app-nav',
+  templateUrl: './nav.component.html',
+  styleUrls: ['./nav.component.css']
+})
+export class NavComponent implements OnInit {
+  model: any = {};
+
+  constructor(private authServce: AuthService) { }
+
+  ngOnInit() {
+  }
+
+  login() {
+    this.authServce.login(this.model).subscribe(next => {
+      console.log('Logged in successfully');
+    }, error => {
+      console.log('Failed to login');
+    });
+  }
+
+  loggedIn() {
+    const token = localStorage.getItem('token');
+    return !!token;
+  }
+
+  logout() {
+    localStorage.removeItem('token');
+    console.log('Logged out');
+  }
+
+}
--- DatingApp.API/Dtos/UserForRegisterDto.cs
+++ DatingApp.API/Dtos/UserForRegisterDto.cs
@@ -5,10 +5,11 @@ namespace DatingApp.API.Dtos
     public class UserForRegisterDto
     {
         [Required]
+        [EmailAddress(ErrorMessage = "Please enter a valid email address.")]
         public string Username { get; set; }
 
         [Required]
-        [StringLength(8, MinimumLength = 4, ErrorMessage = "Password must be between 4 and 8 characters.")]
+        [StringLength(20, MinimumLength = 6, ErrorMessage = "Password must be between 6 to 20 characters long.")]
         public string Password { get; set; }
     }
 }
