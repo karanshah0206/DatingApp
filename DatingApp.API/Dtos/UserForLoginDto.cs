b08ca1dbb9f0b5a91df983f1f7168f0a6276923d
Karan Shah <karanshah0206@outlook.com>
2020-07-18 17:30:08 +0530

    Added Validations To Login DTO

--- DatingApp.API/Dtos/UserForLoginDto.cs
+++ DatingApp.API/Dtos/UserForLoginDto.cs
@@ -1,8 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace DatingApp.API.Dtos
 {
     public class UserForLoginDto
     {
+        [Required]
+        [EmailAddress(ErrorMessage = "Please enter a valid email address.")]
         public string Username { get; set; }
+        [Required]
         public string Password { get; set; }
     }
 }
