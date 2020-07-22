210788840d1a2778cc876bcf89a87316400c748b
Karan Shah <karanshah0206@outlook.com>
2020-07-23 02:03:41 +0530

    Seeding + Transfering User Data

--- DatingApp.API/Dtos/UserForLoginDto.cs
+++ DatingApp.API/Dtos/UserForLoginDto.cs
@@ -5,7 +5,6 @@ namespace DatingApp.API.Dtos
     public class UserForLoginDto
     {
         [Required]
-        [EmailAddress(ErrorMessage = "Please enter a valid email address.")]
         public string Username { get; set; }
         [Required]
         public string Password { get; set; }
---
+++ DatingApp.API/Helpers/AutoMapperProfiles.cs
@@ -0,0 +1,21 @@
+using System.Linq;
+using AutoMapper;
+using DatingApp.API.Dtos;
+using DatingApp.API.Models;
+
+namespace DatingApp.API.Helpers
+{
+    public class AutoMapperProfiles : Profile
+    {
+        public AutoMapperProfiles ()
+        {
+            CreateMap<User, UserForListDto>()
+                .ForMember(dest => dest.PhotoUrl, opt => opt.MapFrom(src => src.Photos.FirstOrDefault(p => p.IsMain).Url))
+                .ForMember(dest => dest.Age, opt => opt.MapFrom(src => src.DateOfBirth.calculateAge()));
+            CreateMap<User, UserForDetailedDto>()
+                .ForMember(dest => dest.Photo, opt => opt.MapFrom(src => src.Photos.FirstOrDefault(p => p.IsMain).Url))
+                .ForMember(dest => dest.Age, opt => opt.MapFrom(src => src.DateOfBirth.calculateAge()));
+            CreateMap<Photo, PhotosForDetailedDto>();
+        }
+    }
+}
