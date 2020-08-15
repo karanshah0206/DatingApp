a67e79138eab7e7cd5119fe9c8f6eee7045b5f5d
Karan Shah <karanshah0206@outlook.com>
2020-08-15 17:57:29 +0530

    Added Filtering Functionality

--- DatingApp.API/Controllers/UsersController.cs
+++ DatingApp.API/Controllers/UsersController.cs
@@ -27,6 +27,13 @@ namespace DatingApp.API.Controllers
 
         [HttpGet]
         public async Task<IActionResult> GetUsers([FromQuery]UserParams userParams) {
+            var currentUserId = int.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value);
+            var userFromRepo = await _repo.GetUser(currentUserId);
+            userParams.UserId = currentUserId;
+            if (string.IsNullOrEmpty(userParams.Gender))
+            {
+                userParams.Gender = userFromRepo.Gender == "male" ? "female" : "male";
+            }
             var users = await _repo.GetUsers(userParams);
             var usersToReturn = _mapper.Map<IEnumerable<UserForListDto>>(users);
             Response.addPagination(users.CurrentPage, users.PageSize, users.TotalCount, users.TotalPages);
--- DatingApp.API/Helpers/UserParams.cs
+++ DatingApp.API/Helpers/UserParams.cs
@@ -10,6 +10,9 @@ namespace DatingApp.API.Helpers
             get { return pageSize; }
             set { pageSize = (value > maxPageSize) ? maxPageSize : value; }
         }
-        
+        public int UserId { get; set; }        
+        public string Gender { get; set; }
+        public int MinAge { get; set; } = 18;
+        public int MaxAge { get; set; } = 99;
     }
 }
