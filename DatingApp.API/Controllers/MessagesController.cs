fe3039f5c4f1ac2accad05cf28fc9b2c993f76c4
Karan Shah <karanshah0206@outlook.com>
2020-08-20 12:28:15 +0530

    Fixed Image Not Autoloading In Chat Bug

--- DatingApp.API/Controllers/MessagesController.cs
+++ DatingApp.API/Controllers/MessagesController.cs
@@ -69,7 +69,9 @@ namespace DatingApp.API.Controllers
         [HttpPost]
         public async Task<IActionResult> CreateMessage(int userId, MessageForCreationDto messageForCreationDto)
         {
-            if (userId != int.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value))
+            var sender = await _repo.GetUser(userId);
+
+            if (sender.Id != int.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value))
                 return Unauthorized();
             
             messageForCreationDto.SenderId = userId;
@@ -83,10 +85,11 @@ namespace DatingApp.API.Controllers
             var message = _mapper.Map<Message>(messageForCreationDto);
 
             _repo.Add(message);
-            var messageToReturn = _mapper.Map<MessageForCreationDto>(message);
 
-            if (await _repo.SaveAll())
+            if (await _repo.SaveAll()) {
+                var messageToReturn = _mapper.Map<MessageToReturnDto>(message);
                 return CreatedAtRoute("GetMessage", new {userId, id = message.Id}, messageToReturn);
+            }
             
             throw new Exception("Failed To Create Message.");
         }
