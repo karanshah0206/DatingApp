acfab2b40e304e4731cb2634da26bd747401629f
Karan Shah <karanshah0206@outlook.com>
2020-08-20 12:46:24 +0530

    Added Delete Functionality To API and SPA

--- DatingApp.API/Controllers/MessagesController.cs
+++ DatingApp.API/Controllers/MessagesController.cs
@@ -93,5 +93,26 @@ namespace DatingApp.API.Controllers
             
             throw new Exception("Failed To Create Message.");
         }
+
+        [HttpPost("{id}")]
+        public async Task<IActionResult> DeleteMessage(int id, int userId)
+        {
+            if (userId != int.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value))
+                return Unauthorized();
+            
+            var messageFromRepo = await _repo.GetMessage(id);
+
+            if (messageFromRepo.SenderId == userId)
+                messageFromRepo.SenderDeleted = true;
+            if(messageFromRepo.RecipientId == userId)
+                messageFromRepo.RecipientDeleted = true;
+            if (messageFromRepo.SenderDeleted && messageFromRepo.RecipientDeleted)
+                _repo.Delete(messageFromRepo);
+            
+            if (await _repo.SaveAll())
+                return NoContent();
+            
+            throw new Exception("Error deleting the message.");
+        }
     }
 }
