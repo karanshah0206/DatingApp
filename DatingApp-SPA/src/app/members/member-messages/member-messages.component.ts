6b76adf3bd0833e22943d4dfcba27c5dcf4a9b2b
Karan Shah <karanshah0206@outlook.com>
2020-08-20 13:00:05 +0530

    Added User Read Feature To Chat

--- DatingApp-SPA/src/app/members/member-messages/member-messages.component.ts
+++ DatingApp-SPA/src/app/members/member-messages/member-messages.component.ts
@@ -3,6 +3,7 @@ import { Message } from '../../_models/message';
 import { UserService } from 'src/app/_services/user.service';
 import { AuthService } from 'src/app/_services/auth.service';
 import { AlertifyService } from 'src/app/_services/alertify.service';
+import { tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-member-messages',
@@ -21,7 +22,14 @@ export class MemberMessagesComponent implements OnInit {
   }
 
   loadMessages() {
-    this.userService.getMessageThread(this.authService.decodedToken.nameid, this.recipientId).subscribe(messages => {
+    const currentUserId = +this.authService.decodedToken.nameid;
+    this.userService.getMessageThread(this.authService.decodedToken.nameid, this.recipientId).pipe(tap(messages => {
+      for (let i = 0; i < messages.length; i++) {
+        if (messages[i].isRead == false && messages[i].recipientId === currentUserId) {
+          this.userService.markAsRead(currentUserId, messages[i].id);
+        }
+      }
+    })).subscribe(messages => {
       this.messages = messages;
     }, error => {
       this.alertify.error(error);
--- DatingApp.API/Controllers/MessagesController.cs
+++ DatingApp.API/Controllers/MessagesController.cs
@@ -114,5 +114,24 @@ namespace DatingApp.API.Controllers
             
             throw new Exception("Error deleting the message.");
         }
+
+        [HttpPost("{id}/read")]
+        public async Task<IActionResult> MarkMessageAsRead(int userId, int id)
+        {
+            if (userId != int.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value))
+                return Unauthorized();
+            
+            var message = await _repo.GetMessage(id);
+
+            if (message.RecipientId != userId)
+                return Unauthorized();
+            
+            message.IsRead = true;
+            message.DateRead = DateTime.Now;
+
+            await _repo.SaveAll();
+
+            return NoContent();
+        }
     }
 }
